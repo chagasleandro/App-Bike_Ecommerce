37f3b75124c08b2c4c9c33b45dce518fd3a4f195
unknown <leandro_srs2012@yahoo.com.br>
2023-11-20 16:02:45 -0300

    src/page

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { Box,NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { theme } from './src/styles/theme';
+import { Home } from './src/page/Home';
+
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <Box flex='1' bg={theme.colors.primary[100]}>
-        
+        <Home />
       </Box>
     </NativeBaseProvider>
   );
---
+++ src/page/Home/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from "native-base";
+import React from "react";
+
+export function Home() {
+    return <Box flex='1' padding='20px' pt='-10px' position='relative'>
+
+    </Box>
+}
